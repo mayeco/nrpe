55a8ee7422cd04fc5dc0cf5bc7aa0dd4189094f9
Mario Young <maye.co@gmail.com>
2021-02-05 00:32:27 -0300

    Initial Commit

--- centos-7-init-server.sh
+++ centos-7-init-server.sh
@@ -3,7 +3,7 @@ set -e
 
 sudo yum remove -y epel-release nrpe nagios-plugins nagios-plugins-all || true
 
-sudo rpm -Uvh https://mirrors.dotsrc.org/fedora-buffet/archive/epel/7.8/x86_64/Packages/e/epel-release-7-12.noarch.rpm
+sudo rpm -Uvh http://mirror.math.princeton.edu/pub/epel/7/x86_64/Packages/e/epel-release-7-13.noarch.rpm
 
 sudo yum --enablerepo=epel -y install nrpe nagios-plugins nagios-plugins-all
 
@@ -19,6 +19,6 @@ sudo chmod +x /usr/lib64/nagios/plugins/check_cpu_perf
 
 sudo curl -fsSL https://raw.githubusercontent.com/mayeco/nrpe/master/nrpe.cfg -o /etc/nagios/nrpe.cfg
 
-sudo chkconfig nrpe on
+sudo systemctl enable nrpe --now
 
-sudo service nrpe start
+sudo systemctl start nrpe
