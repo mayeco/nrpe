8da4e9b90bba31cc5fe910aa6ae800b5fb0f5aa7
Mario Young <maye.co@gmail.com>
2021-02-04 23:18:59 -0300

    update

--- centos-6-init-server.sh
+++ centos-6-init-server.sh
@@ -1,10 +1,11 @@
 #!/bin/sh
 set -e
 
+sudo yum remove -y epel-release nrpe nagios-plugins nagios-plugins-all || true
+
 sudo rpm -Uvh https://mirrors.dotsrc.org/fedora-buffet/archive/epel/6/x86_64/epel-release-6-8.noarch.rpm
 
 sudo yum --enablerepo=epel -y install nrpe nagios-plugins nagios-plugins-all
-
 
 sudo curl -fsSL https://raw.githubusercontent.com/whereisaaron/linux-check-mem-nagios-plugin/master/check_mem \
   -o /usr/lib64/nagios/plugins/check_mem
