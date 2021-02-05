1621a977c23ce5959abc5fdbe198c3e69a8ce263
Mario Young <maye.co@gmail.com>
2021-02-04 23:22:04 -0300

    update

--- centos-6-init-server.sh
+++ centos-6-init-server.sh
@@ -9,9 +9,13 @@ sudo yum --enablerepo=epel -y install nrpe nagios-plugins nagios-plugins-all
 
 sudo curl -fsSL https://raw.githubusercontent.com/whereisaaron/linux-check-mem-nagios-plugin/master/check_mem \
   -o /usr/lib64/nagios/plugins/check_mem
+
+chmod +x /usr/lib64/nagios/plugins/check_mem
+
 sudo curl -fsSL https://raw.githubusercontent.com/skywalka/check-cpu-perf/master/check_cpu_perf.sh \
   -o /usr/lib64/nagios/plugins/check_cpu_perf
 
+chmod +x /usr/lib64/nagios/plugins/check_cpu_perf
 
 sudo curl -fsSL https://raw.githubusercontent.com/mayeco/nrpe/master/nrpe.cfg -o /etc/nagios/nrpe.cfg
 
